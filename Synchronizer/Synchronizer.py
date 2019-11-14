1e2a7913ee88e3cf4bf41b3e8e10d3922e75dd4f
xiancg <chris.granados@xiancg.com>
2019-11-14 18:35:08 -0300

    Setting up pytest

--- Synchronizer/Synchronizer.py
+++ Synchronizer/Synchronizer.py
@@ -1,63 +1,24 @@
 # coding=utf-8
-'''
-Library to copy and compare files and directories.
-Created on Mar 25, 2015
-@author: Chris Granados - Xian chris.granados@xiancg.com http://www.chrisgranados.com/
-'''
 from __future__ import absolute_import, print_function
 
 import os
 import sys
 import shutil
-import time
-from datetime import date
-import logging
 
-logger = logging.getLogger(name='synclog')
+from synchronizer.logger import logger
 
 class Synchronizer(object):
     def __init__(self):
-        pass
+        print("Synchronizer created.")
+    
+    def get_sync_status(self, src_path, trg_path):
+        return 1
 
-def init_logger():
-    logger.setLevel(logging.DEBUG)
-    #Formatter
-    formatter = logging.Formatter(
-                '[%(asctime)s:%(module)s:%(funcName)s:%(lineno)s:%(levelname)s] %(message)s')
-    #STDOUT stream
-    streamHandler = logging.StreamHandler(sys.stdout)
-    streamHandler.setLevel(logging.DEBUG)
-    streamHandler.setFormatter(formatter)
-    logger.addHandler(streamHandler)
-
-def init_file_logger():
-    #Formatter
-    formatter = logging.Formatter(
-                '[%(asctime)s:%(module)s:%(funcName)s:%(lineno)s:%(levelname)s] %(message)s')
-    #Log file stream
-    userPath = os.path.expanduser("~")
-    finalDir = os.path.join(userPath, ".Synchronizer")
-    try:
-        if not os.path.exists(finalDir):
-            os.mkdir(finalDir)
-    except:
-        pass
-    today = date.today()
-    date_string = today.strftime("%d-%m-%Y")
-    log_file_path = os.path.join(finalDir, 'Synchronizer_{}.log'.format(date_string))
-    fileHandler = logging.FileHandler(log_file_path, mode='a')
-    fileHandler.setLevel(logging.DEBUG)
-    fileHandler.setFormatter(formatter)
-    logger.addHandler(fileHandler)
-
-# * Sync session events
-init_logger()
-
-# -------------------------
-# Main
-# -------------------------
+# --------------------------------------------------------
+#  Main
+# --------------------------------------------------------
 def main():
     pass
 
-if __name__ == "__main__":
+if __name__ == "__main__" or 'eclipsePython' in __name__:
     main()
