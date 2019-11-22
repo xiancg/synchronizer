69f6cb0dae8c5bbee18740c1b387e4f42653330b
xiancg <chris.granados@xiancg.com>
2019-11-22 11:44:14 -0300

    First version of get_sync_status test working with files

--- Synchronizer/Synchronizer.py
+++ Synchronizer/Synchronizer.py
@@ -2,67 +2,118 @@
 from __future__ import absolute_import, print_function
 
 import os
-import sys
-import shutil
-import time
+# import sys
+# import shutil
+# import time
+# import filecmp
 
 from synchronizer.logger import logger
 
-# TODO: Revisar flake
 # TODO: Terminar de implementar el primer test
 # TODO: Configurar CI en GitLab
 
 status_dict = {
             1: "In sync",
             2: "Out of sync",
-            3: "Same date, different size",
-            4: "Different date, same size",
-            5: "Both paths missing",
-            6: "Source file missing",
-            7: "Target file missing",
-            8: "Different kind of paths (file-dir, dir-file)"
+            3: "Both paths missing",
+            4: "Source file missing",
+            5: "Target file missing",
+            6: "Different kind of paths (file-dir, dir-file)"
             }
 
 
-def get_sync_status(src_path, trg_path):
+def get_sync_status(
+        src_path, trg_path,
+        ignore_file_name=False,
+        ignore_stats=['st_uid', 'st_gid', 'st_atime', 'st_ctime']):
+    logger_string = "\tSource: {}\n\tTarget: {}\n".format(
+                src_path, trg_path
+                )
     if os.path.exists(src_path) and os.path.exists(trg_path):
-        srcTime = time.ctime(os.path.getmtime(src_path))
-        trgTime = time.ctime(os.path.getmtime(trg_path))
-        srcSize = os.path.getsize(src_path)
-        trgSize = os.path.getsize(trg_path)
-        if os.path.isdir(src_path) and os.path.isdir(trg_path):
-            srcSize = get_dir_size(src_path)
-            trgSize = get_dir_size(trg_path)
-        # In sync
-        if srcTime == trgTime and srcSize == trgSize:
-            return (1, status_dict[1])
-        # Out of sync
-        elif srcTime != trgTime and srcSize != trgSize:
-            return (2, status_dict[2])
-        # Same date, different size
-        elif srcTime == trgTime and srcSize != trgSize:
-            return (3, status_dict[3])
-        # Different date, same size
-        elif srcTime != trgTime and srcSize == trgSize:
-            return (4, status_dict[4])
-    # Both paths missing
+        if os.path.isfile(src_path) and os.path.isfile(trg_path):
+            compare_files = compare_files_stat(
+                src_path, trg_path, ignore_file_name, ignore_stats
+                )
+            result = True
+            logger_string += "\tMatch comparison results:\n"
+            for key, value in compare_files.iteritems():
+                logger_string += "\t\t{}: {}\n".format(key, str(value))
+                if not value:
+                    result = False
+            if result:
+                logger.debug("Files in sync.\n{}".format(logger_string))
+                return (1, status_dict[1])
+            else:
+                logger.debug("Files out of sync.\n{}".format(logger_string))
+                return (2, status_dict[2])
+        elif os.path.isdir(src_path) and os.path.isdir(trg_path):
+            # ! Implement dir comparison functions
+            pass
     elif not os.path.exists(src_path) and not os.path.exists(trg_path):
+        logger.debug("Both given paths do not exist.\n{}".format(
+            logger_string)
+            )
+        return (3, status_dict[3])
+    elif not os.path.exists(src_path) and os.path.exists(trg_path):
+        status = "Source path does not exist but target path does.\n"
+        logger.debug(status + logger_string)
+        return (4, status_dict[4])
+    elif not os.path.exists(trg_path) and not os.path.exists(trg_path):
+        status = "Source path does exist but target path doesn't.\n"
+        logger.debug(status + logger_string)
         return (5, status_dict[5])
-    # Source path missing
-    elif not os.path.exists(src_path):
-        return (6, status_dict[6])
-    # Target path missing
-    elif not os.path.exists(trg_path):
-        return (7, status_dict[7])
-    # Different kind of paths (file-dir)
     elif os.path.isfile(src_path) and os.path.isdir(trg_path):
-        return (8, status_dict[8])
-    # Different kind of paths (dir-file)
+        logger.debug("Different kind of paths (file-dir)\n{}".format(
+            logger_string)
+            )
+        return (6, status_dict[6])
     elif os.path.isdir(trg_path) and os.path.isfile(src_path):
-        return (8, status_dict[8])
-    # Fail
+        logger.debug("Different kind of paths (dir-file)\n{}".format(
+            logger_string)
+            )
+        return (6, status_dict[6])
     else:
+        logger.error("Sync status function failed.\n{}".format(
+            logger_string)
+            )
         return None
+
+
+def compare_files_stat(
+        src_path, trg_path,
+        ignore_file_name=False,
+        ignore_stats=['st_uid', 'st_gid', 'st_atime', 'st_ctime']):
+    src_stat = os.stat(src_path)
+    trg_stat = os.stat(trg_path)
+    stats_dict = {
+        'st_mode': 'Protection bits',
+        'st_ino': 'inode number',
+        'st_dev': 'Device',
+        'st_nlink': 'Number of hard links',
+        'st_uid': 'User id of owner',
+        'st_gid': 'Group id of owner',
+        'st_size': 'File size',
+        'st_atime': 'Most recent access',
+        'st_mtime': 'Last modification',
+        'st_ctime': 'Most recent metadata change'
+    }
+    result = dict()
+    src_file_name = os.path.split(src_path)[1]
+    trg_file_name = os.path.split(trg_path)[1]
+    if src_file_name != trg_file_name and not ignore_file_name:
+        result["File Name"] = False
+    elif src_file_name == trg_file_name and not ignore_file_name:
+        result["File Name"] = True
+
+    for key, value in stats_dict.iteritems():
+        if key not in ignore_stats:
+            if eval("src_stat.{}".format(key)) != \
+               eval("trg_stat.{}".format(key)):
+                result[value] = False
+            else:
+                result[value] = True
+
+    return result
 
 
 def get_dir_size(path):
--- tests/synchronizer_test.py
+++ tests/synchronizer_test.py
@@ -10,11 +10,14 @@ import os
 #  TESTING DATA
 # --------------------------------------------------------
 path_root = os.path.join(
-            os.path.normcase(
-            os.path.abspath(os.path.split(__file__)[0])),
-            "data")
+            os.path.normcase(os.path.abspath(os.path.split(__file__)[0])),
+            "data"
+            )
 path_dir = os.path.join(path_root, "directory", "src_path")
-path_single_file = os.path.join(path_dir, "C_cresta_02__MSH-BUMP.1001.png")
+path_single_file = os.path.join(
+                path_root, "singlefile",
+                "src_path", "C_cresta_02__MSH-BUMP.1001.png"
+                )
 path_missing = os.path.join(path_root, "missingframes", "src_path")
 path_sequence = os.path.join(path_root, "sequence", "src_path")
 path_texture = os.path.join(path_root, "texture", "src_path")
@@ -26,17 +29,36 @@ data_missing = pytest.mark.datafiles(path_missing)
 data_sequence = pytest.mark.datafiles(path_sequence)
 data_texture = pytest.mark.datafiles(path_texture)
 
-# --------------------------------------------------------
-#  TESTS
-# --------------------------------------------------------
 
 class TestClass:
     @data_single_file
-    def test_get_sync_status(self, datafiles):
-        src_path = str(datafiles)
-        trg_path = str(datafiles)
-        result = sync.get_sync_status(src_path, trg_path)
-        assert 1 == result[0], "Sync status is not 1"
+    def test_get_sync_status_files(self, datafiles):
+        # Same file, ignoring last modification
+        src_path = os.path.join(
+            str(datafiles), "C_cresta_02__MSH-BUMP.1001.png"
+            )
+        result = sync.get_sync_status(
+            src_path, trg_path,
+            ignore_stats=[
+                'st_uid', 'st_gid', 'st_atime',
+                'st_ctime', 'st_mtime'
+                ]
+            )
+        assert result is not None
+        if result:
+            assert result[0] == 1, "Sync status is not 1"
+        # Different files, same name
+        dif_trg_path = os.path.join(
+                path_root, "singlefile",
+                "dif_trg_path", "C_cresta_02__MSH-BUMP.1001.png"
+                )
+        result_dif = sync.get_sync_status(src_path, dif_trg_path)
+        assert result_dif is not None
+        if result_dif:
+            assert result_dif[0] == 2, "Sync status is not 2"
+
+    def get_sync_status_dirs(self):
+        pass
 
     def test_get_dir_size(self):
         pass
