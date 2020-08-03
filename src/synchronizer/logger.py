7b9a885c6fa803e0792108710b0a70eae75cbfa5
Chris Granados- Xian <info@chrisgranados.com>
2020-08-03 18:55:58 -0300

    Fixed handlers creation bug and refactored both init loggers functions

--- src/synchronizer/logger.py
+++ src/synchronizer/logger.py
@@ -18,44 +18,78 @@ import os
 import json
 from datetime import date
 
-logger = logging.getLogger(name='synclog')
+_base_name = "synchronizer"
+logger = logging.getLogger(name='{}log'.format(_base_name))
 
 
 def init_logger():
+    """Initialize '{}log' logging object and add a STDOUT handler to output to console, terminal, etc.
+    """.format(_base_name)
     logger.setLevel(logging.DEBUG)
-    # Formatter
-    formatter = logging.Formatter(
-                '[%(asctime)s:%(module)s:%(funcName)s:%(lineno)s:%(levelname)s] %(message)s')  # noqa: E501
-    # STDOUT stream
-    streamHandler = logging.StreamHandler(sys.stdout)
-    streamHandler.setLevel(logging.DEBUG)
-    streamHandler.setFormatter(formatter)
-    logger.addHandler(streamHandler)
+
+    found_handler = False
+    for each in logger.handlers:
+        if isinstance(each, logging.StreamHandler):
+            found_handler = True
+            break
+
+    if not found_handler:
+        # Formatter
+        formatter = logging.Formatter(
+            '[%(asctime)s:%(module)s:%(funcName)s:'
+            '%(lineno)s:%(levelname)s] %(message)s'
+        )
+        # STDOUT stream
+        streamHandler = logging.StreamHandler(sys.stdout)
+        streamHandler.setLevel(logging.DEBUG)
+        streamHandler.setFormatter(formatter)
+        logger.addHandler(streamHandler)
 
 
 def init_file_logger():
-    # Formatter
-    formatter = logging.Formatter(
-                '[%(asctime)s:%(module)s:%(funcName)s:%(lineno)s:%(levelname)s] %(message)s')  # noqa: E501
-    # Log file stream
-    userPath = os.path.expanduser("~")
-    module_dir = os.path.split(__file__)[0]
-    config_location = os.path.join(module_dir, "cfg", "config.json")
-    config = dict()
-    with open(config_location) as fp:
-        config = json.load(fp)
-    finalDir = os.path.join(userPath, "." + config["logger_dir_name"])
+    """Adds file log to '{}log' logging object. Log files will be located at the user OS folder.
 
-    try:
-        if not os.path.exists(finalDir):
-            os.mkdir(finalDir)
-    except OSError:
-        pass
+    Raises:
+        OSError, IOError: Directory for log files couldn't be created.
 
-    today = date.today()
-    date_string = today.strftime("%d-%m-%Y")
-    log_file_path = os.path.join(finalDir, 'sync_{}.log'.format(date_string))
-    fileHandler = logging.FileHandler(log_file_path, mode='a')
-    fileHandler.setLevel(logging.DEBUG)
-    fileHandler.setFormatter(formatter)
-    logger.addHandler(fileHandler)
+    Returns:
+        [str]: Log file path
+    """.format(_base_name)
+
+    found_handler = False
+    for each in logger.handlers:
+        if isinstance(each, logging.FileHandler):
+            found_handler = True
+            break
+
+    if not found_handler:
+        # Formatter
+        formatter = logging.Formatter(
+            '[%(asctime)s:%(module)s:%(funcName)s:'
+            '%(lineno)s:%(levelname)s] %(message)s'
+        )
+        # Log file stream
+        userPath = os.path.expanduser("~")
+        module_dir = os.path.split(__file__)[0]
+        config_location = os.path.join(module_dir, "cfg", "config.json")
+        config = dict()
+        with open(config_location) as fp:
+            config = json.load(fp)
+        finalDir = os.path.join(userPath, "." + config["logger_dir_name"])
+
+        try:
+            if not os.path.exists(finalDir):
+                os.mkdir(finalDir)
+        except (OSError, IOError) as why:
+            raise why
+
+        today = date.today()
+        date_string = today.strftime("%d-%m-%Y")
+        log_file_path = os.path.join(finalDir, '{}_{}.log'.format(_base_name, date_string))
+        fileHandler = logging.FileHandler(log_file_path, mode='a')
+        fileHandler.setLevel(logging.DEBUG)
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+
+        return log_file_path
+    return None
